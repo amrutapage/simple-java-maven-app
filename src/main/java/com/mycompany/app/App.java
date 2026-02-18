4190aa4ae8ec4f0567702b9592837e7c00c17b0f
Amruta Page <amruta.page@Indussoft.com>
2026-02-18 20:03:56 +0530

    commit5

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World-test WebHook4!";
+    private static final String MESSAGE = "Hello World-test WebHook5!";
 
     public App() {}
 
